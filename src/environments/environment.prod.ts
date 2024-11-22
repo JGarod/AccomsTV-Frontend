7063701e06bc229bd1a26906c9d5be2839b948fb
Juan Garod <garode01@gmail.com>
2024-11-21 21:29:42 -0500

    Ajuste produccion env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -10,7 +10,8 @@ let rtmp = 'rtmp://accomz.work.gd/live/'
 export const environment = {
    production: false,
    apiURL: `${ip}:3000/api`, // Cambia esto a la URL de tu API
-   apiURLVideo: `${imgs}:8000`, // DONDE ESTE LO DEL SISTEMA RTMP EN VIDEO
+   // apiURLVideo: `${imgs}:8000`, // DONDE ESTE LO DEL SISTEMA RTMP EN VIDEO
+   apiURLVideo: `${imgs}:8443`, // DONDE ESTE LO DEL SISTEMA RTMP EN VIDEO
    apiURLSockets: `${ip}:3000`, // DONDE SE ALMACENEN LOS SOCKETS
    apiImagenes: `${imgs}:3000`, // DONDE SE ALMACENEN LAS IMAGENES
    apiRTMP: `${rtmp}`, // DONDE SE GESTIONA EL RMP
