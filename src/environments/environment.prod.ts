4e9b519a28c165bca80845418a1b097c9b36e7f8
Juan Garod <garode01@gmail.com>
2024-11-21 21:19:50 -0500

    Ajuste produccion env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 
 let ip = 'https://accomz.work.gd'
-let ipvideo = 'https://ec2-52-90-118-173.compute-1.amazonaws.com'
+// let ipvideo = 'https://ec2-52-90-118-173.compute-1.amazonaws.com'
 let imgs = 'https://accomz.work.gd'
 let rtmp = 'rtmp://accomz.work.gd/live/'
 // let ip = 'https://ec2-52-90-118-173.compute-1.amazonaws.com'
@@ -10,7 +10,7 @@ let rtmp = 'rtmp://accomz.work.gd/live/'
 export const environment = {
    production: false,
    apiURL: `${ip}:3000/api`, // Cambia esto a la URL de tu API
-   apiURLVideo: `${ipvideo}:8000`, // DONDE ESTE LO DEL SISTEMA RTMP EN VIDEO
+   apiURLVideo: `${imgs}:8000`, // DONDE ESTE LO DEL SISTEMA RTMP EN VIDEO
    apiURLSockets: `${ip}:3000`, // DONDE SE ALMACENEN LOS SOCKETS
    apiImagenes: `${imgs}:3000`, // DONDE SE ALMACENEN LAS IMAGENES
    apiRTMP: `${rtmp}`, // DONDE SE GESTIONA EL RMP
