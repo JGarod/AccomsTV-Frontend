181424d1c49e5589c8fba963fec6cf186deb0499
Juan Garod <garode01@gmail.com>
2025-05-08 23:20:17 -0500

    cambio en env para railway

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,19 +1,22 @@
 
-let ip = 'https://accomz.work.gd'
+let ip = 'https://accomstv-production.up.railway.app'
 // let ipvideo = 'https://ec2-52-90-118-173.compute-1.amazonaws.com'
-let imgs = 'https://accomz.work.gd'
-let rtmp = 'rtmp://accomz.work.gd/live/'
+let imgs = 'https://accomstv-production.up.railway.app'
+let rtmp = 'rtmp://accomstv-production.up.railway.app/live/'
 // let ip = 'https://ec2-52-90-118-173.compute-1.amazonaws.com'
 // let imgs = 'https://ec2-52-90-118-173.compute-1.amazonaws.com'
 // let rtmp = 'rtmp://ec2-52-90-118-173.compute-1.amazonaws.com/live/'
 // let ip = 'https://3x1c783j-3000.use2.devtunnels.ms'
 export const environment = {
    production: false,
-   apiURL: `${ip}:3000/api`, // Cambia esto a la URL de tu API
+   apiURL: `${ip}/api`, // Cambia esto a la URL de tu API
+   // apiURL: `${ip}:3000/api`, // Cambia esto a la URL de tu API
    // apiURLVideo: `${imgs}:8000`, // DONDE ESTE LO DEL SISTEMA RTMP EN VIDEO
    apiURLVideo: `${imgs}:8443`, // DONDE ESTE LO DEL SISTEMA RTMP EN VIDEO
-   apiURLSockets: `${ip}:3000`, // DONDE SE ALMACENEN LOS SOCKETS
-   apiImagenes: `${imgs}:3000`, // DONDE SE ALMACENEN LAS IMAGENES
+   apiURLSockets: `${ip}`, // DONDE SE ALMACENEN LOS SOCKETS
+   // apiURLSockets: `${ip}:3000`, // DONDE SE ALMACENEN LOS SOCKETS
+   apiImagenes: `${imgs}`, // DONDE SE ALMACENEN LAS IMAGENES
+   // apiImagenes: `${imgs}:3000`, // DONDE SE ALMACENEN LAS IMAGENES
    apiRTMP: `${rtmp}`, // DONDE SE GESTIONA EL RMP
 
 };
